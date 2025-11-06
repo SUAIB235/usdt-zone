bd48f76f1fc0a855d520eeb0b311ab590c0d5d75
SUYEB <suyebmia00@gmail.com>
2025-11-06 12:39:03 +0600

    Fix styling and spacing in ProductCard component

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -6,7 +6,7 @@ const ProductCard = ({products}) => {
   return (
     <div className='font-mon transition-transform duration-300 hover:scale-105 border border-[#E5E7EB] rounded-2xl bg-[#ffffff]'>
      <img src={products.product_picture} alt={products.title}
-     className='h-[80%] mx-auto w-[80%] object-cover rounded-lg'
+     className=' mx-auto object-cover rounded-lg'
      />
      <div className='flex p-2'>
         <div>
@@ -14,7 +14,7 @@ const ProductCard = ({products}) => {
                 {products.title}
             </h3>
             <div className='flex text-[#f6a355]'><FaStar/> <FaStar/> <FaStar/> <FaStar/> <FaRegStarHalfStroke/></div>
-            <p className='flex gap-2 mb-6'><div className='line-through'>{products.withoutdis}</div> <strong>{products.price}</strong>  BDT</p>
+            <p className='flex gap-2 mb-3'><div className='line-through'>{products.withoutdis}</div> <strong>{products.price}</strong>  BDT</p>
         </div>
      </div>
     </div>
