75db6673e0044c202c6a2e5b50b0af8aae560211
SUYEB <suyebmia00@gmail.com>
2025-11-06 12:38:04 +0600

    Refactor LatestProducts component layout and styles

--- src/components/LatestProducts.jsx
+++ src/components/LatestProducts.jsx
@@ -18,9 +18,11 @@ export default function LatestProducts() {
       .catch((err) => console.error(err));
   }, []);
 
-    return (
+  return (
     <div className=" max-full mx-auto w-11/12 mb-3">
-      <h2 className="text-2xl font-mon font-bold mb-5 text-center text-[#ff8f9c]">Latest Products</h2>
+      <h2 className="text-2xl font-mon font-bold mb-5 text-center text-[#ff8f9c]">
+        Latest Products
+      </h2>
 
       <Swiper
         modules={[Autoplay, Pagination]}
@@ -36,20 +38,26 @@ export default function LatestProducts() {
       >
         {products.map((product) => (
           <SwiperSlide key={product.id}>
-            <div className='font-mon transition-transform duration-300 hover:scale-105 border border-[#E5E7EB] rounded-2xl bg-[#ffffff]'>
-                 <img src={product.product_picture} alt={product.title}
-                 className='h-[80%] mx-auto w-[80%] object-cover rounded-lg'
-                 />
-                 <div className='flex p-2'>
-                    <div>
-                        <h3 className='text-[#21214c] fonr-mon'>
-                            {product.title}
-                        </h3>
-                        <div className='flex text-[#f6a355]'><FaStar/> <FaStar/> <FaStar/> <FaStar/> <FaRegStarHalfStroke/></div>
-                        <p className='flex gap-2 mb-3'><div className='line-through'>{product.withoutdis}</div> <strong>{product.price}</strong>  BDT</p>
-                    </div>
-                 </div>
+            <div className="h-50 md:h-65 font-mon transition-transform duration-300 hover:scale-105 border border-[#E5E7EB] rounded-2xl bg-[#ffffff]">
+              <img
+                src={product.product_picture}
+                alt={product.title}
+                className="h-[120px] w-[120px] lg:h-[70%] mx-auto lg:w-[70%] object-cover rounded-lg"
+              />
+              <div className="flex p-2">
+                <div>
+                  <h3 className="text-[#21214c] fonr-mon">{product.title}</h3>
+                  <div className="flex text-[#f6a355]">
+                    <FaStar /> <FaStar /> <FaStar /> <FaStar />{" "}
+                    <FaRegStarHalfStroke />
+                  </div>
+                  <p className="flex gap-2 mb-3">
+                    <div className="line-through">{product.withoutdis}</div>{" "}
+                    <strong>{product.price}</strong> BDT
+                  </p>
                 </div>
+              </div>
+            </div>
           </SwiperSlide>
         ))}
       </Swiper>
